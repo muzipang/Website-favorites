91d986eacf24d9af49e05f0953bcdec3103373ee
DJ <djmuzipang@163.com>
2019-05-27 23:41:27 +0800

    Modify the basic framework to use webview, the internal UI is subsequently drawn using html

---
+++ code/src/favorites/controller/MainController.java
@@ -0,0 +1,15 @@
+package favorites.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
+public class MainController
+{
+    @FXML
+    private WebView webView;
+
+    public void init()
+    {
+        webView.getEngine().load("https://www.baidu.com/");
+    }
+}
--- code/src/favorites/main/Main.java
+++ code/src/favorites/main/Main.java
@@ -1,7 +1,11 @@
 package favorites.main;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import favorites.controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,8 +15,6 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application
 {
-    private boolean useSysWindowStyle = true;
-
     public static void main(String[] args)
     {
         launch(args);
@@ -22,20 +24,13 @@ public class Main extends Application
     public void start(Stage primaryStage) throws IOException
     {
         FXMLLoader mainWindowLoader;
-        if (useSysWindowStyle)
-        {
-            mainWindowLoader = new FXMLLoader(getClass().getClassLoader().getResource("main-content.fxml"));
-        }
-        else
-        {
-            primaryStage.initStyle(StageStyle.UNDECORATED);
-            mainWindowLoader = new FXMLLoader(getClass().getClassLoader().getResource("main-window.fxml"));
-
-        }
+        mainWindowLoader = new FXMLLoader(getClass().getClassLoader().getResource("main-content.fxml"));
         Parent root = mainWindowLoader.load();
         Scene scene = new Scene(root, 800, 600);
-        scene.getStylesheets().add(getClass().getClassLoader().getResource("main-window.css").toExternalForm());
         primaryStage.setScene(scene);
+
+        MainController windowController = mainWindowLoader.getController();
+        windowController.init();
 
         primaryStage.show();
     }
